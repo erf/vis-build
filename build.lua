44a05be611a33f895c098714cf538ffbc751162a
Erlend Fagerheim <erlendf80@gmail.com>
2017-01-01 03:32:54 +0100

    error message if not a c file

--- build.lua
+++ build.lua
@@ -1,10 +1,14 @@
 function on_win_open(win)
 
+	function error()
+		vis:info('Not a valid c file :/')
+	end
+
 	function build()
 		local f = win.file.name
-		if f == nil then return end
+		if f == nil then error() return end
 		local i = string.find(f, '%.c')
-		if i == nil then return end
+		if i == nil then error() return end
 		local o = string.sub(f, 0, i - 1)
 		vis:info(string.format('building %s', o))
 		vis:command(string.format('!gcc %s -o %s', f, o))
