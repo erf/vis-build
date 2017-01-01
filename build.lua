f2d3652f9378d534a36592b57e3b6e016a12677a
Erlend Fagerheim <erlendf80@gmail.com>
2017-01-01 04:51:30 +0100

    support build swift

--- build.lua
+++ build.lua
@@ -1,17 +1,32 @@
 function on_win_open(win)
 
 	function error()
-		vis:info('Not a valid c file :/')
+		vis:info('This filetype is not supported')
 	end
+	
+	function build_c(f, o)
+		vis:command(string.format('!gcc %s -o %s', f, o))
+	end
+	
+	function build_swift(f, o)
+		vis:command(string.format('!swiftc %s -o %s', f, o))
+	end
+	
+	local lang = {}
+	lang['.c'] = build_c
+	lang['.swift'] = build_swift
 
 	function build()
 		local f = win.file.name
 		if f == nil then error() return end
-		local i = string.find(f, '%.c')
+		local i = string.find(f, '%.')
 		if i == nil then error() return end
-		local o = string.sub(f, 0, i - 1)
-		vis:info(string.format('building %s', o))
-		vis:command(string.format('!gcc %s -o %s', f, o))
+		local ext = string.sub(f, i)
+		local out = string.sub(f, 0, i - 1)
+		local method = lang[ext]
+		if method == nil then error() return end
+		vis:info(string.format('building %s', out))
+		method(f, out)
 	end
 	
 	vis:map(vis.modes.NORMAL, '<C-b>', build)
