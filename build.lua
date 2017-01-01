e6c1801242a79af54e7d063aab09c7900af6e691
Erlend Fagerheim <erlendf80@gmail.com>
2017-01-01 05:39:15 +0100

    support rust, crystal and haskell

--- build.lua
+++ build.lua
@@ -12,9 +12,24 @@ function on_win_open(win)
 		vis:command(string.format('!swiftc %s -o %s', f, o))
 	end
 	
+	function build_rust(f, o)
+		vis:command(string.format('!rustc %s -o %s', f, o))
+	end
+	
+	function build_crystal(f, o)
+		vis:command(string.format('!crystal build %s -o %s', f, o))
+	end
+	
+	function build_haskell(f, o)
+		vis:command(string.format('!ghc %s -o %s', f, o))
+	end
+	
 	local lang = {}
 	lang['.c'] = build_c
 	lang['.swift'] = build_swift
+	lang['.rs'] = build_rust
+	lang['.cr'] = build_crystal
+	lang['.hs'] = build_haskell
 
 	function build()
 		local f = win.file.name
@@ -25,8 +40,9 @@ function on_win_open(win)
 		local method = lang[ext]
 		if method == nil then error() return end
 		local out = string.sub(f, 0, i - 1)
-		vis:info(string.format('building %s', out))
+		vis:info(string.format('building \'%s\'', out))
 		method(f, out)
+		vis:info(string.format('building \'%s\' done', out))
 	end
 	
 	vis:map(vis.modes.NORMAL, '<C-b>', build)
